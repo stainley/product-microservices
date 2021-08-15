16f8e717e6c9d185fb407637abcc697d5bac2999
Stainley Lebron <stainley.lebron@gmail.com>
2021-08-15 14:48:05 -0400

    cobertura plugin

--- util/src/main/java/com/salapp/util/exceptions/InvalidInputException.java
+++ util/src/main/java/com/salapp/util/exceptions/InvalidInputException.java
@@ -2,17 +2,8 @@ package com.salapp.util.exceptions;
 
 public class InvalidInputException extends RuntimeException {
 
-    public InvalidInputException() {}
-
     public InvalidInputException(String message) {
         super(message);
     }
 
-    public InvalidInputException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public InvalidInputException(Throwable cause) {
-        super(cause);
-    }
 }
--- util/src/main/java/com/salapp/util/exceptions/NotFoundException.java
+++ util/src/main/java/com/salapp/util/exceptions/NotFoundException.java
@@ -2,18 +2,8 @@ package com.salapp.util.exceptions;
 
 public class NotFoundException extends RuntimeException {
 
-    public NotFoundException() {
-    }
-
     public NotFoundException(String message) {
         super(message);
     }
 
-    public NotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public NotFoundException(Throwable cause) {
-        super(cause);
-    }
 }
