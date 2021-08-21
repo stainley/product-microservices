a50bccfd7299b2534b1f5483d5a7e6ac976beca5
Stainley Lebron <stainley.lebron@gmail.com>
2021-08-21 15:55:21 -0400

    database integration

--- recommendation/src/test/java/com/salapp/recommendation/RecommendationApplicationTests.java
+++ recommendation/src/test/java/com/salapp/recommendation/RecommendationApplicationTests.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.reactive.server.WebTestClient;
