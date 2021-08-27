c98a14ae4ac34ec904ace043ef4e24f62c8a43f5
Stainley Lebron <stainley.lebron@gmail.com>
2021-08-26 20:00:17 -0400

    Some unit test for Review services

--- product/src/main/java/com/salapp/product/model/ProductEntity.java
+++ product/src/main/java/com/salapp/product/model/ProductEntity.java
@@ -3,13 +3,13 @@ package com.salapp.product.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Version;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.Objects;
-
+@ToString
 @Getter
 @Setter
 @Document(collection = "products")
@@ -33,18 +33,5 @@ public class ProductEntity {
         this.productId = productId;
         this.name = name;
         this.weight = weight;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof ProductEntity)) return false;
-        ProductEntity entity = (ProductEntity) o;
-        return productId == entity.productId && weight == entity.weight && Objects.equals(id, entity.id) && Objects.equals(version, entity.version) && Objects.equals(name, entity.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, version, productId, name, weight);
     }
 }
--- review/src/main/java/com/salapp/review/services/ReviewServiceImpl.java
+++ review/src/main/java/com/salapp/review/services/ReviewServiceImpl.java
@@ -52,17 +52,11 @@ public class ReviewServiceImpl implements ReviewService {
     public List<Review> getReviews(int productId) {
         if (productId < 1) throw new InvalidInputException("Invalid productId: " + productId);
 
-        if (productId == 213) {
-            LOG.debug("No reviews found for productId: {}", productId);
-            return new ArrayList<>();
-        }
+        List<ReviewEntity> entityList = repository.findByProductId(productId);
+        List<Review> list = mapper.entityListToApiList(entityList);
+        list.forEach(e -> e.setServiceAddress(serviceUtil.getServiceAddress()));
 
-        List<Review> list = new ArrayList<>();
-        list.add(new Review(productId, 1, "Author 1", "Subject 1", "Content 1", serviceUtil.getServiceAddress()));
-        list.add(new Review(productId, 2, "Author 2", "Subject 2", "Content 2", serviceUtil.getServiceAddress()));
-        list.add(new Review(productId, 3, "Author 3", "Subject 3", "Content 3", serviceUtil.getServiceAddress()));
-
-        LOG.debug("/reviews response size: {}", list.size());
+        LOG.debug("getReviews: response size: {}", list.size());
 
         return list;
     }
